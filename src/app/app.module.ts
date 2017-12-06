5136732f84e938b907b2e0a5b4c42157e1fbc152
Marshall Munoz <Marshall.Munoz@fbitn.com>
2017-12-06 12:31:59 -0600

    name functionality with piping added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
 
 @NgModule({
   imports: [
-  BrowserModule],
+  BrowserModule,
+  FormsModule
+],
   declarations: [AppComponent, HeroesComponent],
   bootstrap: [AppComponent]
 })
